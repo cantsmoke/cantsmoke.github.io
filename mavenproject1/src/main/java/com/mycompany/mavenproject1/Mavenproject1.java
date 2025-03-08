4f22955140e59fa3c98d2b7f4eec4c2da99384b6
moneyluv <ababaan365@gmail.com>
2025-03-08 10:26:58 +0300

    Создал классы

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.mavenproject1;
 
-/**
- *
- * @author Arseniy
- */
 public class Mavenproject1 {
 
     public static void main(String[] args) {
